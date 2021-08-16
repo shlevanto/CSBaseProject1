df114797d4b2c05a59d0c598855d9816b7b9c067
shlevanto <simo.levanto@helsinki.fi>
2021-08-16 14:24:33 +0300

    message template added to model

--- CSBaseProject1/mymessages/models.py
+++ CSBaseProject1/mymessages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    message_text = models.CharField(max_length=200)
+    sent_date = models.DateTimeField('date sent')
+    sender = models.CharField(null=True, max_length=50)
+    receiver = models.CharField(null=True, max_length=50)
+
